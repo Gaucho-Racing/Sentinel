1d462dfba637feb7fb54c321b4ace25c444f61ab
Bharat Kathi <bharat1031@gmail.com>
2024-05-28 17:28:36 -0700

    updated ping command

--- commands/ping.go
+++ commands/ping.go
@@ -1,15 +1,16 @@
 package commands
 
 import (
+	"sentinel/config"
 	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func Ping(args []string, s *discordgo.Session, m *discordgo.MessageCreate) {
-	message, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
+	message, err := s.ChannelMessageSend(m.ChannelID, "Pong from Sentinel v"+config.Version+"!")
 	if err == nil {
 		delay := message.Timestamp.Sub(m.Timestamp).Milliseconds()
-		s.ChannelMessageEdit(m.ChannelID, message.ID, "Pong! (Delay: "+strconv.FormatInt(delay, 10)+"ms)")
+		s.ChannelMessageEdit(m.ChannelID, message.ID, "Pong from Sentinel v"+config.Version+"!  (**"+strconv.FormatInt(delay, 10)+"ms**)")
 	}
 }
