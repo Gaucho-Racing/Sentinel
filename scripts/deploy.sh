7c0c2625cbe7cf690b0bba83f8b9012b31cda1ab
Bharat Kathi <bharat1031@gmail.com>
2024-08-20 15:29:05 -0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,4 +15,22 @@ fi
 
 echo "Building container for Sentinel v$1"
 # Build the docker container
-docker build -t gauchoracing/sentinel:"$1" -t gauchoracing/sentinel:latest --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push --progress=plain .
+docker build -t gauchoracing/sentinel:"$1" -t gauchoracing/sentinel:latest --platform linux/amd64,linux/arm64 --push --progress=plain .
+
+echo "Container deployed successfully"
+
+# Check if GitHub CLI is installed
+if ! command -v gh &> /dev/null
+then
+    echo "GitHub CLI (gh) is not installed. Please install it to proceed."
+    exit 1
+fi
+
+# Create a release tag
+git tag -s v$1 -m "Release version $1"
+git push origin v$1
+
+# Create a release
+gh release create v$1 --generate-notes
+
+echo "Package released successfully for version $1"
