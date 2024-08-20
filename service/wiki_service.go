632df0dee33946b80fb6d4d9127be81a86bdfee0
Bharat Kathi <bharat1031@gmail.com>
2024-08-20 14:11:00 -0700

    added better error handling to wiki service

--- service/wiki_service.go
+++ service/wiki_service.go
@@ -4,169 +4,168 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"sentinel/config"
 	"sentinel/model"
 	"sentinel/utils"
 )
 
-func GetAllWikiUsers() []model.WikiUser {
+func GetAllWikiUsers() ([]model.WikiUser, error) {
 	var userResponse model.WikiArrayResponse[model.WikiUser]
 	var users []model.WikiUser
 
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://wiki.gauchoracing.com/api/users", nil)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error creating request: %v", err)
-		return users
+		utils.SugarLogger.Errorln(err)
+		return users, err
 	}
 	req.Header.Set("Authorization", "Token "+config.WikiToken)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error making request: %v", err)
-		return users
+		utils.SugarLogger.Errorln(err)
+		return users, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		utils.SugarLogger.Errorf("Unexpected status code: %d", resp.StatusCode)
-		return users
+		body, _ := io.ReadAll(resp.Body)
+		return users, fmt.Errorf("failed to get all wiki users: %s", string(body))
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(&userResponse)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error decoding response: %v", err)
-		return users
+		utils.SugarLogger.Errorln(err)
+		return users, err
 	}
 	users = userResponse.Data
 
-	return users
+	return users, nil
 }
 
-func GetWikiUserByID(id int) model.WikiUser {
+func GetWikiUserByID(id int) (model.WikiUser, error) {
 	var user model.WikiUser
 
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://wiki.gauchoracing.com/api/users/%d", id), nil)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error creating request: %v", err)
-		return user
+		utils.SugarLogger.Errorln(err)
+		return user, err
 	}
 	req.Header.Set("Authorization", "Token "+config.WikiToken)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error making request: %v", err)
-		return user
+		utils.SugarLogger.Errorln(err)
+		return user, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		utils.SugarLogger.Errorf("Unexpected status code: %d", resp.StatusCode)
-		return user
+		body, _ := io.ReadAll(resp.Body)
+		return user, fmt.Errorf("failed to get wiki user: %s", string(body))
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(&user)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error decoding response: %v", err)
-		return user
+		utils.SugarLogger.Errorln(err)
+		return user, err
 	}
-	return user
+	return user, nil
 }
 
-func CreateWikiUser(input model.WikiUserCreate) int {
+func CreateWikiUser(input model.WikiUserCreate) (int, error) {
 	jsonData, err := json.Marshal(input)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error marshaling input: %v", err)
-		return 0
+		utils.SugarLogger.Errorln(err)
+		return 0, err
 	}
 
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", "https://wiki.gauchoracing.com/api/users", bytes.NewBuffer(jsonData))
 	if err != nil {
-		utils.SugarLogger.Errorf("Error creating request: %v", err)
-		return 0
+		utils.SugarLogger.Errorln(err)
+		return 0, err
 	}
 	req.Header.Set("Authorization", "Token "+config.WikiToken)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error making request: %v", err)
-		return 0
+		utils.SugarLogger.Errorln(err)
+		return 0, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		utils.SugarLogger.Errorf("Unexpected status code: %d", resp.StatusCode)
-		return 0
+		body, _ := io.ReadAll(resp.Body)
+		return 0, fmt.Errorf("failed to create wiki user: %s", string(body))
 	}
 
 	var user model.WikiUser
 	err = json.NewDecoder(resp.Body).Decode(&user)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error decoding response: %v", err)
-		return 0
+		utils.SugarLogger.Errorln(err)
+		return 0, err
 	}
 	utils.SugarLogger.Infof("Created wiki user: %s (%s)", user.Name, user.Email)
-	return user.ID
+	return user.ID, nil
 }
 
-func UpdateWikiUser(id int, input model.WikiUserCreate) bool {
+func UpdateWikiUser(id int, input model.WikiUserCreate) error {
 	jsonData, err := json.Marshal(input)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error marshaling input: %v", err)
-		return false
+		utils.SugarLogger.Errorln(err)
+		return err
 	}
 
 	client := &http.Client{}
 	req, err := http.NewRequest("PUT", fmt.Sprintf("https://wiki.gauchoracing.com/api/users/%d", id), bytes.NewBuffer(jsonData))
 	if err != nil {
-		utils.SugarLogger.Errorf("Error creating request: %v", err)
-		return false
+		utils.SugarLogger.Errorln(err)
+		return err
 	}
 	req.Header.Set("Authorization", "Token "+config.WikiToken)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error making request: %v", err)
-		return false
+		utils.SugarLogger.Errorln(err)
+		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		utils.SugarLogger.Errorf("Unexpected status code: %d", resp.StatusCode)
-		return false
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to update wiki user: %s", string(body))
 	}
-
-	return true
+	return nil
 }
 
-func DeleteWikiUser(id int) bool {
+func DeleteWikiUser(id int) error {
 	client := &http.Client{}
 	req, err := http.NewRequest("DELETE", fmt.Sprintf("https://wiki.gauchoracing.com/api/users/%d", id), nil)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error creating request: %v", err)
-		return false
+		utils.SugarLogger.Errorln(err)
+		return err
 	}
 	req.Header.Set("Authorization", "Token "+config.WikiToken)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		utils.SugarLogger.Errorf("Error making request: %v", err)
-		return false
+		utils.SugarLogger.Errorln(err)
+		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusNoContent {
-		utils.SugarLogger.Errorf("Unexpected status code: %d", resp.StatusCode)
-		return false
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to delete wiki user: %s", string(body))
 	}
-
-	return true
+	return nil
 }
 
-func CreateWikiUserWithPassword(password string, userID string) int {
+func CreateWikiUserWithPassword(password string, userID string) error {
 	user := GetUserByID(userID)
 	if user.ID == "" {
-		return 0
+		return fmt.Errorf("user not found")
 	}
 	roles := []int{int(model.WikiRoleEditor)}
 	if user.IsInnerCircle() {
@@ -180,12 +179,12 @@ func CreateWikiUserWithPassword(password string, userID string) int {
 		Password:       password,
 		SendInvite:     false,
 	}
-	id := CreateWikiUser(input)
-	if id == 0 {
-		return 0
+	id, err := CreateWikiUser(input)
+	if err != nil {
+		return err
 	}
 	er := GetRolesForUser(userID)
 	er = append(er, "wiki_"+fmt.Sprint(id))
 	SetRolesForUser(userID, er)
-	return id
+	return nil
 }
