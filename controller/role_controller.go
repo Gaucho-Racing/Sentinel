87ac1133f4e0357eddaca094535c20c7ea53b2a5
Bharat Kathi <bharat1031@gmail.com>
2024-08-29 17:22:36 -0700

    updated role endpoints

--- controller/role_controller.go
+++ controller/role_controller.go
@@ -7,16 +7,23 @@ import (
 )
 
 func GetAllRolesForUser(c *gin.Context) {
-	RequireAny(c, RequestTokenHasScope(c, "sentinel:all"), RequestTokenHasScope(c, "user:read"))
-	RequireAny(c, RequestUserHasID(c, c.Param("userID")), RequestUserHasRole(c, "d_admin"))
+	Require(c, Any(
+		RequestTokenHasScope(c, "sentinel:all"),
+		All(
+			RequestTokenHasScope(c, "user:read"),
+			Any(RequestUserHasID(c, c.Param("userID")), RequestUserHasRole(c, "d_admin")),
+		),
+	))
 
 	roles := service.GetRolesForUser(c.Param("userID"))
 	c.JSON(200, roles)
 }
 
 func SetRolesForUser(c *gin.Context) {
-	RequireAny(c, RequestTokenHasScope(c, "sentinel:all"))
-	RequireAny(c, RequestUserHasID(c, c.Param("userID")), RequestUserHasRole(c, "d_admin"))
+	Require(c, All(
+		RequestTokenHasScope(c, "sentinel:all"),
+		RequestUserHasRole(c, "d_admin"),
+	))
 
 	var roles []string
 	if err := c.ShouldBindJSON(&roles); err != nil {
