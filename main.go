c47cb0977b76b205a6b6008589a1139a957093ae
Bharat Kathi <bharat1031@gmail.com>
2025-01-30 18:16:57 -0800

    forgot to uncomment important code

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"sentinel/commands"
 	"sentinel/config"
+	"sentinel/controller"
 	"sentinel/database"
+	"sentinel/jobs"
 	"sentinel/service"
 	"sentinel/utils"
 )
@@ -23,16 +25,14 @@ func main() {
 	go service.SyncRolesForAllUsers()
 	commands.InitializeDiscordBot()
 
-	service.PopulateMemberDirectorySheet()
+	jobs.RegisterDriveCronJob()
+	jobs.RegisteGithubCronJob()
+	jobs.RegisterDiscordCronJob()
 
-	// jobs.RegisterDriveCronJob()
-	// jobs.RegisteGithubCronJob()
-	// jobs.RegisterDiscordCronJob()
-
-	// router := controller.SetupRouter()
-	// controller.InitializeRoutes(router)
-	// err := router.Run(":" + config.Port)
-	// if err != nil {
-	// 	utils.SugarLogger.Fatalln(err)
-	// }
+	router := controller.SetupRouter()
+	controller.InitializeRoutes(router)
+	err := router.Run(":" + config.Port)
+	if err != nil {
+		utils.SugarLogger.Fatalln(err)
+	}
 }
