deccc8bc206d50600feb3943c011caf1116e555f
Bharat Kathi <bharat.kathi@withpersona.com>
2026-03-18 20:41:33 -0700

    feat(core): add Application model

---
+++ core/model/application.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type Application struct {
+	ID           string    `json:"id" gorm:"primaryKey"`
+	OwnerID      string    `json:"owner_id" gorm:"index"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	ClientID     string    `json:"client_id" gorm:"uniqueIndex"`
+	ClientSecret string    `json:"-"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
+}
+
+func (Application) TableName() string {
+	return "application"
+}
