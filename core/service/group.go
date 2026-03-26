5861660dbf2cf19b9fec6147f328bdacf0733fd5
Bharat Kathi <bharat1031@gmail.com>
2026-03-25 23:49:39 -0700

    feat(core): add group member and owner service functions

--- core/service/group.go
+++ core/service/group.go
@@ -45,3 +45,70 @@ func DeleteGroup(id string) error {
 	}
 	return nil
 }
+
+func GetMembersForGroup(groupID string) ([]model.GroupMember, error) {
+	var members []model.GroupMember
+	if err := database.DB.Where("group_id = ?", groupID).Find(&members).Error; err != nil {
+		return []model.GroupMember{}, err
+	}
+	return members, nil
+}
+
+func GetGroupMember(groupID string, entityID string) (model.GroupMember, error) {
+	var member model.GroupMember
+	if err := database.DB.Where("group_id = ? AND entity_id = ?", groupID, entityID).First(&member).Error; err != nil {
+		return model.GroupMember{}, err
+	}
+	return member, nil
+}
+
+func CreateGroupMember(member model.GroupMember) (model.GroupMember, error) {
+	if err := database.DB.Create(&member).Error; err != nil {
+		return model.GroupMember{}, err
+	}
+	return member, nil
+}
+
+func UpdateGroupMember(member model.GroupMember) (model.GroupMember, error) {
+	if err := database.DB.Save(&member).Error; err != nil {
+		return model.GroupMember{}, err
+	}
+	return member, nil
+}
+
+func DeleteGroupMember(groupID string, entityID string) error {
+	if err := database.DB.Where("group_id = ? AND entity_id = ?", groupID, entityID).Delete(&model.GroupMember{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetOwnersForGroup(groupID string) ([]model.GroupOwner, error) {
+	var owners []model.GroupOwner
+	if err := database.DB.Where("group_id = ?", groupID).Find(&owners).Error; err != nil {
+		return []model.GroupOwner{}, err
+	}
+	return owners, nil
+}
+
+func GetGroupOwner(groupID string, entityID string) (model.GroupOwner, error) {
+	var owner model.GroupOwner
+	if err := database.DB.Where("group_id = ? AND entity_id = ?", groupID, entityID).First(&owner).Error; err != nil {
+		return model.GroupOwner{}, err
+	}
+	return owner, nil
+}
+
+func CreateGroupOwner(owner model.GroupOwner) (model.GroupOwner, error) {
+	if err := database.DB.Create(&owner).Error; err != nil {
+		return model.GroupOwner{}, err
+	}
+	return owner, nil
+}
+
+func DeleteGroupOwner(groupID string, entityID string) error {
+	if err := database.DB.Where("group_id = ? AND entity_id = ?", groupID, entityID).Delete(&model.GroupOwner{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
