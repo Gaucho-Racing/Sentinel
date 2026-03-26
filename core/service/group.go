3cdee0a87642ac0fda1deccfba3aa77039772b46
Bharat Kathi <bharat1031@gmail.com>
2026-03-25 23:50:08 -0700

    feat(core): add group join request service functions

--- core/service/group.go
+++ core/service/group.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/gaucho-racing/sentinel/core/database"
 	"github.com/gaucho-racing/sentinel/core/model"
+	"github.com/gaucho-racing/sentinel/core/pkg/logger"
 	"github.com/gaucho-racing/ulid-go"
 )
 
@@ -108,6 +109,96 @@ func CreateGroupOwner(owner model.GroupOwner) (model.GroupOwner, error) {
 
 func DeleteGroupOwner(groupID string, entityID string) error {
 	if err := database.DB.Where("group_id = ? AND entity_id = ?", groupID, entityID).Delete(&model.GroupOwner{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetJoinRequestsByGroup(groupID string) ([]model.GroupJoinRequest, error) {
+	var requests []model.GroupJoinRequest
+	if err := database.DB.Where("group_id = ?", groupID).Find(&requests).Error; err != nil {
+		return []model.GroupJoinRequest{}, err
+	}
+	for i := range requests {
+		PopulateJoinRequest(&requests[i])
+	}
+	return requests, nil
+}
+
+func GetJoinRequestsByEntity(entityID string) ([]model.GroupJoinRequest, error) {
+	var requests []model.GroupJoinRequest
+	if err := database.DB.Where("entity_id = ?", entityID).Find(&requests).Error; err != nil {
+		return []model.GroupJoinRequest{}, err
+	}
+	for i := range requests {
+		PopulateJoinRequest(&requests[i])
+	}
+	return requests, nil
+}
+
+func GetJoinRequestByID(id string) (model.GroupJoinRequest, error) {
+	var request model.GroupJoinRequest
+	if err := database.DB.Where("id = ?", id).First(&request).Error; err != nil {
+		return model.GroupJoinRequest{}, err
+	}
+	PopulateJoinRequest(&request)
+	return request, nil
+}
+
+func CreateJoinRequest(request model.GroupJoinRequest) (model.GroupJoinRequest, error) {
+	if request.ID == "" {
+		request.ID = ulid.Make().Prefixed("gjr")
+	}
+	if err := database.DB.Create(&request).Error; err != nil {
+		return model.GroupJoinRequest{}, err
+	}
+	PopulateJoinRequest(&request)
+	return request, nil
+}
+
+func UpdateJoinRequest(request model.GroupJoinRequest) (model.GroupJoinRequest, error) {
+	if err := database.DB.Save(&request).Error; err != nil {
+		return model.GroupJoinRequest{}, err
+	}
+	PopulateJoinRequest(&request)
+	return request, nil
+}
+
+func DeleteJoinRequest(id string) error {
+	if err := database.DB.Where("id = ?", id).Delete(&model.GroupJoinRequest{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func PopulateJoinRequest(request *model.GroupJoinRequest) {
+	comments, err := GetCommentsForJoinRequest(request.ID)
+	if err != nil {
+		logger.SugarLogger.Errorf("Failed to get comments for join request %s: %v", request.ID, err)
+	}
+	request.Comments = comments
+}
+
+func GetCommentsForJoinRequest(requestID string) ([]model.GroupJoinRequestComment, error) {
+	var comments []model.GroupJoinRequestComment
+	if err := database.DB.Where("request_id = ?", requestID).Find(&comments).Error; err != nil {
+		return []model.GroupJoinRequestComment{}, err
+	}
+	return comments, nil
+}
+
+func CreateJoinRequestComment(comment model.GroupJoinRequestComment) (model.GroupJoinRequestComment, error) {
+	if comment.ID == "" {
+		comment.ID = ulid.Make().Prefixed("gjrc")
+	}
+	if err := database.DB.Create(&comment).Error; err != nil {
+		return model.GroupJoinRequestComment{}, err
+	}
+	return comment, nil
+}
+
+func DeleteJoinRequestComment(id string) error {
+	if err := database.DB.Where("id = ?", id).Delete(&model.GroupJoinRequestComment{}).Error; err != nil {
 		return err
 	}
 	return nil
