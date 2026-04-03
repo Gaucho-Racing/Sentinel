321426d71e21d49d93bffcd8073e0dec369c6111
Bharat Kathi <bharat1031@gmail.com>
2026-04-03 12:32:52 -0700

    feat(discord): wire up entity lookup via core external auth endpoint

--- discord/service/entity.go
+++ discord/service/entity.go
@@ -1,9 +1,24 @@
 package service
 
+import (
+	"github.com/gaucho-racing/sentinel/discord/pkg/logger"
+	"github.com/gaucho-racing/sentinel/discord/pkg/sentinel"
+)
+
+type entityResponse struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
+
 // GetEntityIDForDiscordUser resolves a Discord user ID to a Sentinel entity ID.
 // Returns "" if no mapping is found, allowing callers to persist records
 // with an empty entity_id that can be backfilled later.
 func GetEntityIDForDiscordUser(discordUserID string) string {
-	// TODO: implement lookup via EntityExternalAuth
-	return ""
+	var entity entityResponse
+	err := sentinel.Get("/core/entity/external/DISCORD/"+discordUserID, &entity)
+	if err != nil {
+		logger.SugarLogger.Debugf("No entity found for Discord user %s: %v", discordUserID, err)
+		return ""
+	}
+	return entity.ID
 }
