93289f9c6349ecd32cd8466f466857522778afdc
Bharat Kathi <bharat1031@gmail.com>
2026-04-15 00:00:18 -0700

    refactor(core): consolidate InitializeDefaultApplications into application.go

--- core/service/application.go
+++ core/service/application.go
@@ -7,7 +7,40 @@ import (
 	"github.com/gaucho-racing/sentinel/core/model"
 	"github.com/gaucho-racing/sentinel/core/pkg/logger"
 	"github.com/gaucho-racing/ulid-go"
+	"gorm.io/gorm"
 )
+
+const SentinelClientID = "sentinel"
+
+func InitializeDefaultApplications() {
+	_, err := GetApplicationByClientID(SentinelClientID)
+	if err == gorm.ErrRecordNotFound {
+		app, err := CreateApplication(model.Application{
+			Name:        "Sentinel",
+			Description: "Sentinel Identity Platform",
+			ClientID:    SentinelClientID,
+		})
+		if err != nil {
+			logger.SugarLogger.Fatalf("Failed to create Sentinel application: %v", err)
+			return
+		}
+		logger.SugarLogger.Infof("Created Sentinel application (id=%s, client_id=%s)", app.ID, app.ClientID)
+		logger.SugarLogger.Infof("Sentinel client secret: %s", app.ClientSecret)
+
+		defaultRedirectURIs := []string{
+			"http://localhost:3000/auth/callback",
+			"https://sso.gauchoracing.com/auth/callback",
+		}
+		for _, uri := range defaultRedirectURIs {
+			CreateApplicationRedirectURI(app.ID, uri)
+		}
+		logger.SugarLogger.Infof("Added %d default redirect URIs", len(defaultRedirectURIs))
+	} else if err != nil {
+		logger.SugarLogger.Fatalf("Failed to check for Sentinel application: %v", err)
+	} else {
+		logger.SugarLogger.Infoln("Sentinel application already exists")
+	}
+}
 
 func GetAllApplications() ([]model.Application, error) {
 	var applications []model.Application
