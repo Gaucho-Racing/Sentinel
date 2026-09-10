248924dee74b0c3b2acb428b362edc1c4958c1c2
Bharat Kathi <bharat.kathi@withpersona.com>
2026-09-09 21:39:10 -0700

    release: sentinel v5.11.1

--- core/config/config.go
+++ core/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 const Name = "sentinel-core"
-const Version = "5.11.0"
+const Version = "5.11.1"
 
 func FormattedNameWithVersion() string {
 	return Name + ":v" + Version
--- saml/config/config.go
+++ saml/config/config.go
@@ -5,7 +5,7 @@ import (
 )
 
 const Name = "sentinel-saml"
-const Version = "5.11.0"
+const Version = "5.11.1"
 
 func FormattedNameWithVersion() string {
 	return Name + ":v" + Version
